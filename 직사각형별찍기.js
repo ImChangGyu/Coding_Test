98ec74afe030a6dca7eadbfb08c50b35c13d6e4f
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2021-05-17 16:56:29 +0900

    [update]직사각형별찍기.js

--- 직사각형별찍기.js
+++ 직사각형별찍기.js
@@ -1,8 +1,8 @@
 process.stdin.setEncoding("utf8");
 process.stdin.on("data", (data) => {
     const a = data.split(" ");
-    const n = Number(a[0]);
-    const m = Number(a[1]);
+    const n = Number(a[0]),
+        m = Number(a[1]);
 
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
@@ -13,3 +13,20 @@ process.stdin.on("data", (data) => {
         console.log();
     }
 });
+//보고 감탄한 코드
+/* 
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', data => {
+    const n = data.split(" ");
+    const a = Number(n[0]), b = Number(n[1]);
+    const row = '*'.repeat(a)
+    for(let i =0; i < b; i++){
+        console.log(row)
+    }
+
+});
+*/
+//위의 코드는 다른 사람의 풀이에서 확인한 코드인데
+//매우 간결해 놀랐다.
+//repeat이라는 메서드를 이용해 가로의 길이 만큼
+//*를 작성하게 만들었다.
